d14b0e323c3e069882c5432425b8dd7221ffe794
stimur1709 <77297261+stimur1709@users.noreply.github.com>
2026-08-30 21:14:14 +0700

    noop

--- src/main/java/com/github/stimur1709/cloudops/task/TaskType.java
+++ src/main/java/com/github/stimur1709/cloudops/task/TaskType.java
@@ -1,21 +1,9 @@
 package com.github.stimur1709.cloudops.task;
 
-import com.github.stimur1709.cloudops.probe.ProbeType;
-
 public enum TaskType {
-    HTTP_CHECK(ProbeType.HTTP_CHECK),
-    PORT_CHECK(ProbeType.PORT_CHECK),
-    DNS_CHECK(ProbeType.DNS_CHECK),
-    PING(ProbeType.PING),
-    TLS_CHECK(ProbeType.TLS_CHECK);
-
-    private final ProbeType probeType;
-
-    TaskType(ProbeType probeType) {
-        this.probeType = probeType;
-    }
-
-    public ProbeType probeType() {
-        return probeType;
-    }
+    HTTP_CHECK,
+    PORT_CHECK,
+    DNS_CHECK,
+    PING,
+    TLS_CHECK
 }
